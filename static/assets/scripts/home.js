00da872d4642d75048fcc16b0d02c5dabffa0d2f
Yea <129461157+voxel1@users.noreply.github.com>
2024-02-18 10:43:40 -0800

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -59,10 +59,10 @@ document.onkeydown = function (evt) {
 }
 
 let splashtext = [
-  'Over 7 Million Users in 2023!',
-  'Fastest growing proxy server!',
-  'Made by Bubbo!',
-  'Check out discord.gg/interstellar :)',
+  'Nah',
+  'Hi',
+  'Omg you have the password!',
+  'Yippe!',
 ]
 
 document.getElementById('splash').innerText = splashtext[Math.floor(Math.random() * splashtext.length)]

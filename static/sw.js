0ff5b11a1c5f861ee2608800a1d4b6d2180680c5
Yea.. <129461157+voxel1@users.noreply.github.com>
2024-11-21 15:33:27 -0600

    Delete static directory

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('/m/bundle.js')
-importScripts('/m/config.js')
-importScripts('/m/sw.js')
-
-const sw = new UVServiceWorker()
-let userKey = new URL(location).searchParams.get('userkey')
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)))
